2cffc08e08557617fd5e8638fd262618a9448885
KichuMohan <kichumohan779@gmail.com>
2020-06-26 00:54:24 +0530

    Updates

---
+++ precache-manifest.00937b90a22f90997e5613a5ce63a900.js
@@ -0,0 +1,118 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "341c9832c3a7305ec76302394656814b",
+    "url": "/tone/index.html"
+  },
+  {
+    "revision": "84c7bcd6f5ee00084944",
+    "url": "/tone/static/css/main.7c3a96b6.chunk.css"
+  },
+  {
+    "revision": "356d32d383e4d7a3c58e",
+    "url": "/tone/static/js/2.5f3dc6ae.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tone/static/js/2.5f3dc6ae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "84c7bcd6f5ee00084944",
+    "url": "/tone/static/js/main.a9271b6c.chunk.js"
+  },
+  {
+    "revision": "4686d29a28f45f0cdd12",
+    "url": "/tone/static/js/runtime-main.45f41f24.js"
+  },
+  {
+    "revision": "2099506cb996afb4c48a96912fa8ecd8",
+    "url": "/tone/static/media/404.2099506c.png"
+  },
+  {
+    "revision": "72fce59ec01dbe905c1ef689df97c7c7",
+    "url": "/tone/static/media/bass1.72fce59e.mp3"
+  },
+  {
+    "revision": "6712aed5b74b3b6ab5d8bb5443f8cc04",
+    "url": "/tone/static/media/bass2.6712aed5.mp3"
+  },
+  {
+    "revision": "2ab1726ae3eea60624ed25fa12dca31a",
+    "url": "/tone/static/media/bass3.2ab1726a.mp3"
+  },
+  {
+    "revision": "06ae9d60b6bce69f23eaf2d52a1926de",
+    "url": "/tone/static/media/china1.06ae9d60.mp3"
+  },
+  {
+    "revision": "04ed3819efed8c1e5bb7dda439520ca9",
+    "url": "/tone/static/media/china2.04ed3819.mp3"
+  },
+  {
+    "revision": "45991dbb1143673313fce98edbadb34f",
+    "url": "/tone/static/media/china3.45991dbb.mp3"
+  },
+  {
+    "revision": "97f191dbf44978988eb6707c5c2d10a2",
+    "url": "/tone/static/media/china4.97f191db.mp3"
+  },
+  {
+    "revision": "46261bde6c85c7b26c281fd492caaf48",
+    "url": "/tone/static/media/china5.46261bde.mp3"
+  },
+  {
+    "revision": "0951d22fccf7df96cc9a28648509962e",
+    "url": "/tone/static/media/crash1.0951d22f.mp3"
+  },
+  {
+    "revision": "0730cbe3bbe1101270992e2825cd3bfa",
+    "url": "/tone/static/media/crash2.0730cbe3.mp3"
+  },
+  {
+    "revision": "e37cfed6fb65956c3db7fa3b6480fc96",
+    "url": "/tone/static/media/drum1.e37cfed6.mp3"
+  },
+  {
+    "revision": "9e4f3fa0822b3c3a4874d59d0386775a",
+    "url": "/tone/static/media/drum2.9e4f3fa0.mp3"
+  },
+  {
+    "revision": "dcc5b3b3203567c914da9a285c45b42e",
+    "url": "/tone/static/media/hithat1.dcc5b3b3.mp3"
+  },
+  {
+    "revision": "9561adc447f62371f2766b3ef9dd32d8",
+    "url": "/tone/static/media/medium1.9561adc4.mp3"
+  },
+  {
+    "revision": "0cb200dfbe7805bec4affd7a50680531",
+    "url": "/tone/static/media/medium2.0cb200df.mp3"
+  },
+  {
+    "revision": "c3d55c9897a94029b40045dbf9b00d60",
+    "url": "/tone/static/media/page.c3d55c98.wav"
+  },
+  {
+    "revision": "823acee5c0750289886883bdef5c4cd5",
+    "url": "/tone/static/media/ride1.823acee5.mp3"
+  },
+  {
+    "revision": "4e5f171a4d72f3da54c2eb598d5db5fc",
+    "url": "/tone/static/media/snare1.4e5f171a.mp3"
+  },
+  {
+    "revision": "33742fac04c761dfabdcef43cd358196",
+    "url": "/tone/static/media/snare2.33742fac.mp3"
+  },
+  {
+    "revision": "6257fa7ded25ec0b9947d631737e2b9c",
+    "url": "/tone/static/media/snare3.6257fa7d.mp3"
+  },
+  {
+    "revision": "cfc43b0def27e4e90494ff1245fdebe9",
+    "url": "/tone/static/media/snare4.cfc43b0d.mp3"
+  },
+  {
+    "revision": "d1877e8cf60af770fed2e2e33bd6bab0",
+    "url": "/tone/static/media/splash1.d1877e8c.mp3"
+  }
+]);
